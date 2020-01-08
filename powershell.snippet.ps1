ff677b8716046e0ae93869146cd479f16e93ff1f
joetrapp <>
2020-01-07 21:50:37 -0800

    Add PS snippets

--- powershell.snippet.ps1
+++ powershell.snippet.ps1
@@ -1,11 +1,12 @@
 #### ForEach loop from CSV
-
 $CSV = Import-CSV -Path .\filename.csv -Delimiter "," -Header HeaderName
-
-ForEach ($line in $CSV) {
-
-}
+ForEach ($line in $CSV) {}
 
 #### Alias with Select-Object
-
 Select-Object -Property @{N='$Alias';E={$_.$OriginalName}}
+
+#### Get last boot time
+Get-CimInstance -ClassName win32_operatingsystem | Select-Object LastBootUpTime
+
+#### Get directory size
+(Get-ChildItem c:\Downloads -Recurse -File | Measure-Object Length -s).Sum /1GB
